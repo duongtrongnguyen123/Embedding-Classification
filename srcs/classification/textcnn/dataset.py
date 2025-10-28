dc54ff5c1d5c58a163a1d8c1ec5fd925a7eb0c89
Nguyen Dang Khoi <khoind16102006@gmail.com>
2025-10-28 19:53:05 +0700

    done

--- srcs/classification/textcnn/dataset.py
+++ srcs/classification/textcnn/dataset.py
@@ -1,6 +1,10 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent.parent.parent))  # Thêm Embedding-Classification/
+
 import torch
 from torch.utils.data import Dataset
-from src.data_utils import encode_text
+from srcs.classification.textcnn.data_utils import encode_text
 
 class ReviewDataset(Dataset):
     def __init__(self, texts, labels, word2id, max_len=300):
